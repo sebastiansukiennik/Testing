c576c06c22576fea5ed5d66ea6405414ea646106
Sebastian <ssukiennik@student.agh.edu.pl>
2021-03-02 19:44:27 +0100

    1 zmiana

--- TESTING/main.c
+++ TESTING/main.c
@@ -10,6 +10,6 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    printf("Tutaj witamy nowo powstały program ze swiatem\n");
     return 0;
 }
