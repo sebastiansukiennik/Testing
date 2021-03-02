932a9f3fccd3bf3d84e3be31bf0e2640c3436570
Sebastian <ssukiennik@student.agh.edu.pl>
2021-03-02 19:42:19 +0100

    Initial Commit

---
+++ TESTING/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TESTING
+//
+//  Created by Sebastian Sukiennik on 02/03/2021.
+//  Copyright © 2021 Sebastian Sukiennik. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
